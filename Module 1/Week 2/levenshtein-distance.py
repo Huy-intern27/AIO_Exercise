2bc70745a2adcb80178d5b90436199cfb2cfd12f
Huy-intern27 <tranhuysuxt@gmail.com>
2024-06-16 20:53:36 +0700

    Fix sonar config

--- Module 1/Week 2/levenshtein-distance.py
+++ Module 1/Week 2/levenshtein-distance.py
@@ -1,9 +1,9 @@
 def levenshtein(word1, word2):
-    distance = [[0] * (len(word2) + 1) for i in range(len(word1) + 1)]
+    distance = [[0] * (len(word2) + 1) for _ in range(len(word1) + 1)]
 
-    for i in range(0, len(word1)):
+    for i in range(0, len(word1) + 1):
         distance[i][0] = i
-    for j in range(0, len(word2)):
+    for j in range(0, len(word2) + 1):
         distance[0][j] = j
 
     for i in range(1, len(word1) + 1):
@@ -11,13 +11,13 @@ def levenshtein(word1, word2):
             if word1[i - 1] == word2[j - 1]:
                 distance[i][j] = distance[i - 1][j - 1]
             else:
-                distance[i][j] = 1 + min(distance[i - 1][j], distance[i][j - 1], distance[i - 1][j - 1])
+                distance[i][j] = 1 + min(distance[i - 1][j],
+                                         distance[i][j - 1], distance[i - 1][j - 1])
 
     return distance[len(word1)][len(word2)]
+
 
 if __name__ == "__main__":
     word1 = input()
     word2 = input()
     print(levenshtein(word1, word2))
-
-
